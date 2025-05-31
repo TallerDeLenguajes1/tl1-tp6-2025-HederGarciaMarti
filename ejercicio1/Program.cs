ec482bee1d7daa231c3c00a7de8ac0a22a5884d2
Heder <hedersafe.2016@gmail.com>
2025-05-31 15:33:19 -0300

    Termine el ejercicio1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Invertir
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número: ");
+        string variable = Console.ReadLine();
+        bool valor = int.TryParse(variable, out int numero);
+        int invertir = 0, num;
+        if (valor)
+        {
+            if (numero > 0)
+            {
+                while (numero > 0)
+                {
+                    num = numero % 10;
+                    invertir = invertir * 10 + num;
+                    numero = numero / 10;
+                }
+                Console.WriteLine($"Número invertido: {invertir}");
+            }
+            else
+            {
+                Console.WriteLine("El número debe ser mayor a 0.");
+            }
+        }
+    }
+}
